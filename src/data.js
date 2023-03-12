2c264d986652fe27d07b2939571acaac313ccba2
Shadab khan <112720913+Shadabkhan0809@users.noreply.github.com>
2023-03-12 16:06:49 +0530

    main

--- src/data.js
+++ src/data.js
@@ -1,90 +1,148 @@
-const connection = require('./connector')
-const data = require('./data');
-const bookData = data.book;
-const userData = data.user;
+let book = [
+    {
+        title: "Book1",
+        description: "description1",
+        genre: "genre1",
+        author: "author1",
+        rating: 3,
+        review: "review1",
+        FavQuotes: "FavQuotes1",
+        section: '0'
 
+    },
+    {
+        title: "Book2",
+        description: "description2",
+        genre: "genre2",
+        author: "author2",
+        rating: 3,
+        review: "review2",
+        FavQuotes: "FavQuotes2",
+        section: '0'
 
-const initialize = async () => {
+    },
+    {
+        title: "Book3",
+        description: "description3",
+        genre: "genre3",
+        author: "author3",
+        rating: 3,
+        review: "review3",
+        FavQuotes: "FavQuotes3",
+        section: '0'
 
-    await
-        new Promise((res, rej) => {
-            connection.query("DROP TABLE IF EXISTS Books;",
-                (err, result) => {
-                    if (err) rej(err);
-                    else res();
-                });
-        })
+    },
+    {
+        title: "Book4",
+        description: "description4",
+        genre: "genre4",
+        author: "author4",
+        rating: 3,
+        review: "review4",
+        FavQuotes: "FavQuotes4",
+        section: '0'
 
-    await
-        new Promise((res, rej) => {
-            connection.query("CREATE TABLE Books (id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255) NOT NULL,description VARCHAR(255),genre VARCHAR(255), author VARCHAR(255), rating INT, review VARCHAR(255), FavQuotes VARCHAR(255), section ENUM('0' ,'1', '2' ));",
-                (err, result) => {
-                    if (err) {
-                        rej(err)
-                        // console.log(err)
-                    }
-                    else {
-                        res();
-                    }
-                });
-        })
-    for (let a in bookData) {
-        let element = bookData[a];
-        await new Promise((res, rej) => {
-            connection.query("INSERT INTO Books (title, description, genre, author, rating, review, FavQuotes, section) VALUES (?,?,?,?,?,?,?,?);", [element.title, element.description, element.genre, element.author, element.rating, element.review, element.FavQuotes, element.section], (err, result) => {
-                if (err) {
-                    rej(err)
-                    // console.log(err)
-                }
-                else {
-                    res();
-                }
-            })
-        });
+    },
+    {
+        title: "Book5",
+        description: "description5",
+        genre: "genre5",
+        author: "author5",
+        rating: 3,
+        review: "review5",
+        FavQuotes: "FavQuotes5",
+        section: '0'
+
+    },
+    {
+        title: "Book6",
+        description: "description6",
+        genre: "genre6",
+        author: "author6",
+        rating: 3,
+        review: "review6",
+        FavQuotes: "FavQuotes6",
+        section: '1'
+
+    },
+    {
+        title: "Book7",
+        description: "description7",
+        genre: "genre7",
+        author: "author7",
+        rating: 3,
+        review: "review7",
+        FavQuotes: "FavQuotes7",
+        section: '2'
+
+    },
+    {
+        title: "Book8",
+        description: "description8",
+        genre: "genre8",
+        author: "author8",
+        rating: 3,
+        review: "review8",
+        FavQuotes: "FavQuotes8",
+        section: '2'
+
+    },
+    {
+        title: "Book9",
+        description: "description9",
+        genre: "genre9",
+        author: "author9",
+        rating: 3,
+        review: "review9",
+        FavQuotes: "FavQuotes9",
+        section: '1'
+
+    },
+    {
+        title: "Book10",
+        description: "description10",
+        genre: "genre10",
+        author: "author10",
+        rating: 3,
+        review: "review10",
+        FavQuotes: "FavQuotes10",
+        section: '0'
+
+    },
+    {
+        title: "Book11",
+        description: "description11",
+        genre: "genre11",
+        author: "author11",
+        rating: 3,
+        review: "review11",
+        FavQuotes: "FavQuotes11",
+        section: '2'
+
+    },
+    {
+        title: "Book12",
+        description: "description12",
+        genre: "genre12",
+        author: "author12",
+        rating: 3,
+        review: "review12",
+        FavQuotes: "FavQuotes12",
+        section: '1'
+
+    },
+];
+let user = [
+    {
+        name: "Hitesh",
+        toRead: 'Book5,Book9',
+        isRead: 'Book2,Book3'
+    },
+    {
+        name: "Dinesh",
+        toRead: 'Book6,Book8',
+        isRead: 'Book2,Book1'
     }
+]
 
-    await
-        new Promise((res, rej) => {
-            connection.query("DROP TABLE IF EXISTS user;",
-                (err, result) => {
-                    if (err) rej(err);
-                    else res();
-                });
-        })
-
-    await
-        new Promise((res, rej) => {
-            connection.query("CREATE TABLE user (id varchar(100) PRIMARY KEY,isRead VARCHAR(1000),toRead varchar(1000)); ",
-                (err, result) => {
-                    if (err) {
-                        rej(err)
-                        // console.log(err)
-                    }
-                    else {
-                        res();
-                    }
-                });
-        })
-    for (let a in userData) {
-        let element = userData[a];
-        await new Promise((res, rej) => {
-            connection.query("INSERT INTO user (id, isRead, toRead) VALUES (?,?,?);", [element.name, element.isRead, element.toRead], (err, result) => {
-                if (err) {
-                    rej(err)
-                    // console.log(err)
-                }
-                else {
-                    res();
-                }
-            })
-        });
-
-    }
-    connection.end(() => {
-        console.log('Connection Closed')
-    })
-}
-initialize();
-
-
-module.exports = connection;
+module.exports = { book, user };
